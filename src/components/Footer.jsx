3c65a591bb03bd1f2594cdd1942e58f9bc99c479
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-10-31 19:20:45 -0400

    modify footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 
-export default function Footer (props) {
+export default function Footer () {
 
     return (
     <footer>
         <div className="footerInfo">
-            <a target="_blank" href="https://x.com/ParsaMh" style={{margin : 20}}>X /></a>
-            <a target="_blank" href="https://github.com/parsamh8" style={{margin : 20}}>Github /></a>
-            <a target="_blank" href="https://www.linkedin.com/in/parsamh/" style={{margin : 20}}>Linkedin /></a>
+            <a target="_blank" href="https://x.com/ParsaMh" style={{margin : 20}}>X</a>
+            <a target="_blank" href="https://github.com/parsamh8" style={{margin : 20}}>Github</a>
+            <a target="_blank" href="https://www.linkedin.com/in/parsamh/" style={{margin : 20}}>Linkedin</a>
         </div>
     </footer>
     )
