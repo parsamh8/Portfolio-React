558d79143bf0432a61a1520e49fe47ad56743ff4
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-12-03 13:06:43 -0500

    modify content @ home page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -11,6 +11,7 @@ export default function Body() {
 
                 <div style={{ padding: 35 }}>
                     <h1>Hi! I'm <span>Parsa</span></h1>
+                    <h1>Full Stack Developer</h1>
                     <h2>Welcome to my portfolio</h2>
                 </div>
             </ Animated>
