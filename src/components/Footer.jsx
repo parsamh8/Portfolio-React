d4927ac5779da9696fa5e1dad54339c76acdf2bc
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2025-02-11 23:23:39 -0500

    modify footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,15 @@ export default function Footer () {
     return (
     <footer>
         <div className="footerInfo">
+        <a className="vt323" target="_blank" href="/" >Parsa McMoody</a>
             <a className="vt323" target="_blank" href="https://x.com/ParsaMh" >X</a>
-            <a className="vt323" target="_blank" href="https://github.com/parsamh8" >Github</a>
-            <a className="vt323" target="_blank" href="https://www.linkedin.com/in/parsamh/" >Linkedin</a>
+            <a className="vt323"
+                href="mailto:parsamh8@gmail.com?subject=Friend Inquiries&body=How can I help you, my friend?"
+              >
+                parsamh8[at]gmail.com
+              </a>
+            {/* <a className="vt323" target="_blank" href="https://github.com/parsamh8" >Github</a>
+            <a className="vt323" target="_blank" href="https://www.linkedin.com/in/parsamh/" >Linkedin</a> */}
         </div>
         <h6 className="center green">2025 &copy; <a href="/">Parsamh</a>. All right reserved </h6>
     </footer>
